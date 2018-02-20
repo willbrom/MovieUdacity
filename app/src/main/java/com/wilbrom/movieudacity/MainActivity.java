6e4386a6db8021faa45e5c995e14802f2c83bd76
willbrom <alisajid552@yahoo.com>
2018-02-20 16:27:04 +0300

    Add: SettingActivity
    Included SettingActivity with MainActivity as its parent

--- app/src/main/java/com/wilbrom/movieudacity/MainActivity.java
+++ app/src/main/java/com/wilbrom/movieudacity/MainActivity.java
@@ -2,11 +2,15 @@ package com.wilbrom.movieudacity;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.wilbrom.movieudacity.adapters.MovieListAdapter;
 import com.wilbrom.movieudacity.models.Movies;
@@ -19,6 +23,7 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements MovieListAdapter.MovieItemInteractionListener {
 
+    private View container;
     private RecyclerView movieList;
     private MovieListAdapter adapter;
 
@@ -27,11 +32,14 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        container = (FrameLayout) findViewById(R.id.container);
         movieList = (RecyclerView) findViewById(R.id.movie_list);
+
         movieList.setLayoutManager(new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false));
 
         adapter = new MovieListAdapter(this);
         movieList.setAdapter(adapter);
+
         URL url = NetworkUtils.getUrl(NetworkUtils.POPULAR);
         new MovieAsyncTask().execute(url);
     }
@@ -39,8 +47,27 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
     @Override
     public void onClickMovieItem(Results results) {
         Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra("par", results);
+        intent.putExtra(Intent.EXTRA_TEXT, results);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_setting:
+                startActivity(new Intent(this, SettingActivity.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public class MovieAsyncTask extends AsyncTask<URL, Void, Movies> {
@@ -51,7 +78,6 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
 
             try {
                 String res = NetworkUtils.getHttpResponse(urls[0]);
-                Log.d("TAG", res);
                 movies = JsonUtils.parseMovieJson(res);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -63,10 +89,9 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         @Override
         protected void onPostExecute(Movies movies) {
             if (movies != null) {
-                Log.d("TAG", "Page: " + movies.getPage());
                 adapter.setResultsList(movies.getResults());
             } else {
-                Log.d("TAG", "Some thing went wrong");
+                Snackbar.make(container, getString(R.string.error_occurred), Snackbar.LENGTH_INDEFINITE).show();
             }
         }
     }
