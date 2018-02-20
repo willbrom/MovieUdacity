c07e0d5f49f1b4a7928bc65370db2caefffa33c9
willbrom <alisajid552@yahoo.com>
2018-02-20 12:21:28 +0300

    Update: Detail screen
    Add more movie info(overview, backdrop, vote average, release date). Update layout(Coordinator layout, CardView)

--- app/src/main/java/com/wilbrom/movieudacity/DetailActivity.java
+++ app/src/main/java/com/wilbrom/movieudacity/DetailActivity.java
@@ -2,13 +2,22 @@ package com.wilbrom.movieudacity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.wilbrom.movieudacity.models.Results;
+import com.wilbrom.movieudacity.utilities.NetworkUtils;
 
 public class DetailActivity extends AppCompatActivity {
 
     private TextView title;
+    private TextView overview;
+    private TextView releaseDate;
+    private TextView voteAverage;
+    private ImageView backdrop;
+
+    private Results results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +25,21 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         title = (TextView) findViewById(R.id.title);
-        Results results = (Results) getIntent().getParcelableExtra("par");
+        overview = (TextView) findViewById(R.id.overview);
+        releaseDate = (TextView) findViewById(R.id.release_date);
+        voteAverage = (TextView) findViewById(R.id.vote_average);
+        backdrop = (ImageView) findViewById(R.id.backdrop);
+
+
+        results = (Results) getIntent().getParcelableExtra("par");
+
         title.setText(results.getTitle());
+        overview.setText(results.getOverView());
+        releaseDate.setText(results.getReleaseDate());
+        voteAverage.setText(String.valueOf(results.getVoteAverage()));
+
+        Picasso.with(this)
+                .load(NetworkUtils.getImageUrl(NetworkUtils.IMAGE_SIZE_w780) + results.getBackdropPath())
+                .into(backdrop);
     }
 }
