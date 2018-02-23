80c74ffc57acc2c64c3e1698804d0d21505899ef
Will <alisajid552@yahoo.com>
2018-02-23 22:57:01 +0300

    Add: Favorite functionality
    Flagging a movie as favorite add's it to the database using the ContentProvider.

--- app/src/main/java/com/wilbrom/movieudacity/DetailActivity.java
+++ app/src/main/java/com/wilbrom/movieudacity/DetailActivity.java
@@ -1,16 +1,23 @@
 package com.wilbrom.movieudacity;
 
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.wilbrom.movieudacity.data.MovieContract;
 import com.wilbrom.movieudacity.models.Results;
 import com.wilbrom.movieudacity.utilities.NetworkUtils;
 
@@ -22,6 +29,7 @@ public class DetailActivity extends AppCompatActivity {
     private TextView voteAverage;
     private ImageView backdrop;
     private ImageView poster;
+    private View parentView;
 
     private Results results;
 
@@ -30,6 +38,7 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        parentView = (CoordinatorLayout) findViewById(R.id.coordinator);
         title = (TextView) findViewById(R.id.title);
         overview = (TextView) findViewById(R.id.overview);
         releaseDate = (TextView) findViewById(R.id.release_date);
@@ -67,5 +76,45 @@ public class DetailActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickFav(View view) {
+        Uri uri = getContentResolver().insert(MovieContract.ResultsEntry.CONTENT_URI, getContentValues());
+
+        if (uri != null)
+            Snackbar.make(parentView, "Data inserted", Snackbar.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "Data inserted", Toast.LENGTH_SHORT).show();
+    }
+
+    private ContentValues getContentValues() {
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_VOTE_COUNT, results.getVoteCount());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_MOVIE_ID, results.getId());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_VIDEO, results.isVideo());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_VOTE_AVERAGE, results.getVoteAverage());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_TITLE, results.getTitle());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_POPULARITY, results.getPopularity());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_POSTER_PATH, results.getPosterPath());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_ORIGINAL_LANGUAGE, results.getOriginalLanguage());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_ORIGINAL_TITLE, results.getOriginalTitle());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_GENRE_IDS, genreIdToString());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_BACKDROP_PATH, results.getBackdropPath());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_ADULT, results.isAdult());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_OVERVIEW, results.getOverView());
+        contentValues.put(MovieContract.ResultsEntry.COLUMN_RELEASE_DATE, results.getReleaseDate());
+
+        return contentValues;
+    }
+
+    private String genreIdToString() {
+        String genreIds = "";
+
+        for (int i : results.getGenreIds()) {
+            genreIds += String.valueOf(i) + " ";
+        }
+
+        return genreIds;
     }
 }
